264c23f895ba4a6dad58210e3d94cb8425c81f86
Saiuk-dev <sairajukarande@gmail.com>
2026-07-27 11:08:13 +0530

    Print Substring

--- String/Str.java
+++ String/Str.java
@@ -4,6 +4,14 @@ import java.util.*;
 public class Str {
     public static void main(String[] args) {
        String name = "Tony Stark";
-       System.out.println(name.length());
+       System.out.println(Substring(name, 0, 7));
+    }
+
+    public static String Substring(String name , int si , int ei){
+        String substr="";
+        for(int i=si ; i<ei; i++){
+            substr += name.charAt(i);
+        }
+        return substr;
     }
 }
