00978e02fae381f93ec16e47d452c5fcb6fc7fa9
Saiuk-dev <sairajukarande@gmail.com>
2026-08-11 19:44:12 +0530

    Print number is decreasing order

---
+++ Recursion/Printreverse.java
@@ -0,0 +1,17 @@
+package Recursion;
+
+public class Printreverse {
+    public static void main(String[] args){
+        int n =10;
+        printdec(n);
+    }
+
+    public static void printdec(int n){
+        if(n==1){
+            System.out.println(n);
+            return;
+        }
+        System.out.print(n+" ");
+        printdec(n-1);
+    }
+}
